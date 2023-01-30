16a12dc2daab789f0b9ec1133d280fabff665812
Alsace7004 <alabibamidele14@gmail.com>
2023-01-30 18:56:05 +0100

    users module apperance working on

--- resources/js/views/Users.vue
+++ resources/js/views/Users.vue
@@ -5,9 +5,63 @@
 
             <div class="data_box">
                 <div class="data_box_header">The data box header</div>
+                <!-- Table Content Begin -->
                 <div class="data_box_content">
-                    The data box content
+                    <!-- The data box content -->
+                    <table>
+                        <thead>
+                            <th>#</th>
+                            <th>Nom</th>
+                            <th>Prenom</th>
+                            <th>Action</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>01</td>
+                                <td>Kodjo</td>
+                                <td>Mawugnon</td>
+                                <td>Deuxieme</td>
+                            </tr>
+                            <tr>
+                                <td>02</td>
+                                <td>Komlan</td>
+                                <td>Mawuegnigan</td>
+                                <td>Premier</td>
+                            </tr>
+                            <tr>
+                                <td>03</td>
+                                <td>Kokou</td>
+                                <td>Okeke</td>
+                                <td>Deuxieme</td>
+                            </tr>
+                            <tr>
+                                <td>04</td>
+                                <td>Yao</td>
+                                <td>Mawulolo</td>
+                                <td>Premier</td>
+                            </tr>
+                            <tr>
+                                <td>05</td>
+                                <td>Koffi</td>
+                                <td>Selom</td>
+                                <td>Deuxieme</td>
+                            </tr>
+                            <tr>
+                                <td>06</td>
+                                <td>Komi</td>
+                                <td>Esse</td>
+                                <td>Premier</td>
+                            </tr>
+                            <tr>
+                                <td>07</td>
+                                <td>Kossi</td>
+                                <td>Alla</td>
+                                <td>Deuxieme</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+                <!-- Table Content End -->
                 <div class="data_box_footer">The data box footer</div>
             </div>
         </div>
@@ -35,7 +89,7 @@ export default {
         .data_box{
             border: 1px solid red;
             width: 100%;
-
+            /* border-radius: 5px; */
         }
         .data_box_header{
             margin-bottom: 2px solid #fff;
@@ -46,5 +100,14 @@ export default {
         }
         .data_box_footer{
             background-color: magenta;
+        }
+        /*****************TABLE-DESIGN*****************/
+        table{
+            text-align: center;
+            width: 100%;
+        }
+        table thead th{
+            text-transform: capitalize;
+            border: 1px solid red;
         }
 </style>
