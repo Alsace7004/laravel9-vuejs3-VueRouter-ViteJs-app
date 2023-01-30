fc3794bbe628409d9c663d428c7c53024467a127
Alsace7004 <alabibamidele14@gmail.com>
2023-01-30 18:30:03 +0100

    begin working on users module

--- resources/js/views/Users.vue
+++ resources/js/views/Users.vue
@@ -2,6 +2,14 @@
         <div class="users">
             <h3>Hello, Am The User View</h3>
             <h5>Je suis la vue: User</h5>
+
+            <div class="data_box">
+                <div class="data_box_header">The data box header</div>
+                <div class="data_box_content">
+                    The data box content
+                </div>
+                <div class="data_box_footer">The data box footer</div>
+            </div>
         </div>
 </template>
 
@@ -19,8 +27,24 @@ export default {
             color: #fff;
             display: flex;
             flex-direction: column;
-            justify-content: center;
-            align-items: center;
+           /*  justify-content: center;
+            align-items: center; */
             height: 86.45vh;
+            padding: 25px;
+        }
+        .data_box{
+            border: 1px solid red;
+            width: 100%;
+
+        }
+        .data_box_header{
+            margin-bottom: 2px solid #fff;
+            background-color: maroon;
+        }
+        .data_box_content{
+            background-color: rosybrown;
+        }
+        .data_box_footer{
+            background-color: magenta;
         }
 </style>
