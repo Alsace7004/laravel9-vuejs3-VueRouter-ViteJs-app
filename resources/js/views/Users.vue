cb1036acd55b92a43109d7fea8222dc31b2d46d2
Alsace7004 <alabibamidele14@gmail.com>
2023-02-13 13:43:00 +0100

    modal set up

--- resources/js/views/Users.vue
+++ resources/js/views/Users.vue
@@ -2,7 +2,7 @@
         <div class="users">
             <h3>Users</h3>
             <div style="margin-bottom:1rem">
-                <button class="excel_export_btn" @click="showModal">Show Modal</button>
+                <button class="excel_export_btn" @click="show_Modal">Show Modal</button>
                 <button class="excel_export_btn" @click="dialogState = true">Open Dialog</button>
             </div>
             
@@ -51,7 +51,7 @@
                 <!-- data box footer -->
             </div>
             <!-- Proper Modal Begin -->
-            <proper-modal v-show="isModalVisible">
+            <proper-modal v-show="isModalVisible" modalName="user">
                 <template v-slot:header>
                     <h4>The Modal title</h4>
                     <button type="button" @click="closeModal()" class="close" data-dismiss="modal" aria-label="Close">
@@ -59,7 +59,7 @@
                     </button>
                 </template>
                 <template v-slot:body>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Nunc sed velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus proin nibh nisl condimentum. 
                         Fringilla urna porttitor rhoncus dolor purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id. 
                         Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. 
@@ -68,7 +68,7 @@
                         Tincidunt eget nullam non nisi est sit amet facilisis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. 
                         Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. 
                         Nec nam aliquam sem et tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a. Elementum tempus egestas sed sed. 
-                        Faucibus purus in massa tempor nec feugiat nisl pretium fusce.</p>
+                        Faucibus purus in massa tempor nec feugiat nisl pretium fusce.</p> -->
                     <p>Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. 
                         Ut faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. 
                         Eu non diam phasellus vestibulum lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. 
@@ -148,6 +148,14 @@ import ProperModal from "../components/ProperModal.vue"
         length :'5',
     })
     /*********************************/
+    const show_Modal = ()=>{
+      /*  let k = document.querySelector("#users")
+       if(k){
+        isModalVisible.value = true
+       }
+       return  */  
+               $("#user").modal("show")
+    }
     const goPrev = (ppu)=>{
         //alert("The NPU is:"+ppu)
         axios.get(ppu,{params:tData}).then((res)=>{
@@ -172,10 +180,11 @@ import ProperModal from "../components/ProperModal.vue"
     }
     /*********************************/
     const showModal = ()=>{
-        isModalVisible.value = true
+        //isModalVisible.value = true
     }
     const closeModal = ()=>{
-        isModalVisible.value = false
+        //isModalVisible.value = false
+        $("#user").modal("hide")
     }
     /*********************************/
     onMounted(()=>{
