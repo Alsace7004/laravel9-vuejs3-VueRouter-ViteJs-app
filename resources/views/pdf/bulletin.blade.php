35bab7b1ad147578adc76517175ebe57e8870c3e
alsace7004 <alabibamidele14@gmail.com>
2023-09-04 06:37:12 +0200

    bulletin en quelque sorte en forme

--- resources/views/pdf/bulletin.blade.php
+++ resources/views/pdf/bulletin.blade.php
@@ -8,7 +8,7 @@
       Bulletin
     </title>
     <style>
-        .container_1{
+          .container_1{
             font-size: 0;
             text-align: justify;
             }
@@ -24,6 +24,29 @@
                 font-size: 14px;
                 text-align: left;
             }
+
+            
+
+            
+            /*  */
+            .container_2{
+              display: table;
+              width: 100%;
+            }
+            .table_row{
+              display: table-row;
+              height: 20px;
+            }
+            .table_cell{
+              border: 1px solid #000;
+              /* background-color: lightsalmon; */
+              display: table-cell;
+              padding: 2px;
+            
+              width: 50%;
+            }
+
+          
     </style>
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
@@ -31,7 +54,7 @@
   </head>
   <body class="" style="border:1px solid red;width:100vw;font-size:10px">
     
-    <div class="invoice p-5 mb-3" style="border:1px solid green;">
+    <div class="invoice p-5 mb-3" style="">
       <div class="row mb-5 mt-5">
         <div class="col-12">
             <div class="container_1">
@@ -201,16 +224,74 @@
       
       <div class="row">
         <div class="col-12">
-            <div class="container_1">
-                <div class="class-1">
-                    <i class="fas fa-globe"></i><small style="display: block;"><strong>Moyenne du Trimestre 1 :</strong>18.47</small> 
-                    <i class="fas fa-globe"></i><small style="display: block;"><strong>Année Scolaire :</strong>  2022-2023</small>
+
+              {{-- new --}}
+              
+              <div class="container_2">
+                <div class="table_row">
+                  <div class="table_cell">
+                    <div class="container_2">
+                      <div class="table_row">
+                        <div class="table_cell">
+                          <small style="text-transform:uppercase;display:block;text-align:center;background-color:gray;font-size:10px"><strong>Moyennes </strong></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;position: relative">Moy. du Trimestre 1 :<span style="position:absolute;right:0;font-weight:bold">18.47</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;position: relative">Moy. du Trimestre 2  :<span style="position:absolute;right:0;font-weight:bold">18.47</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;position: relative">Moy. du Trimestre 3 :<span style="position:absolute;right:0;font-weight:bold">18.47</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;position: relative">Moy.        General :<span style="position:absolute;right:0;font-weight:bold">18.47</span></small> 
+                        </div>
+                        <div class="table_cell">
+                          <small style="text-transform:uppercase;display:block;text-align:center;background-color:gray;font-size:10px"><strong>Classements </strong></small> 
+
+                          <small style="text-transform:uppercase;font-size:9px;display:inline-block;width:100%;">CLASSEMENT :                                    <span style="text-transform:none;float:right;font-weight:bold">1 er</span></small> 
+                          <small style="text-transform:uppercase;font-size:9px;display:inline-block;width:100%;position: relative;">CLASSEMENT  :                <span style="text-transform:none;position:absolute;right:0;font-weight:bold">2 eme</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;display:inline-block;width:100%;">CLASSEMENT :                      <span style="text-transform:none;float:right;font-weight:bold">3 eme</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;display:inline-block;width:100%;position: relative;">CLASSEMENT :   <span style="text-transform:none;position:absolute;right:0;font-weight:bold">4 eme</span></small> 
+                        </div>
+                      </div>
+                    </div> 
+                  </div>
+                  <div class="table_cell">
+                    <small style="text-transform:uppercase;display:block;text-align:center;background-color:gray;font-size:10px" ><strong>Observation du Chef d'Etablissement </strong></small> 
+                    <small style="margin:1rem 0px 1rem 0px;text-transform:uppercase;display:block;font-size:9px">Travail  :<span style="font-weight:bold;">........................</span></small> 
+                    <small style="text-transform:uppercase;display:block;font-size:9px">Conduite :<span style="font-weight:bold">........................</span></small> 
+                  </div>
                 </div>
-                <div class="class-2">
-                    <small style="display: block;" ><strong>Classement</strong>1 er</small> 
-                    <small style="display: block;" ><strong>Effectif :</strong>  5</small>
+              </div>
+              {{--  --}}
+              <div class="container_2">
+                <div class="table_row">
+                  <div class="table_cell">
+                    <div class="container_2">
+                      <div class="table_row">
+                        <div class="table_cell">
+                          <small style="text-transform:uppercase;display:block;text-align:center;background-color:gray;font-size:10px"><strong>Bilan de la classe </strong></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;position: relative">MOYENNE LA PLUS FORTE :<span style="position:absolute;right:0;font-weight:bold">18.47</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;position: relative">MOYENNE LA PLUS FAIBLE  :<span style="position:absolute;right:0;font-weight:bold">18.47</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;position: relative">MOYENNE DE LA CLASSE :<span style="position:absolute;right:0;font-weight:bold">18.47</span></small> 
+                          <small style="text-transform:uppercase;display:block;text-align:center;background-color:gray;font-size:10px"><strong>Décision du Conseil </strong></small> 
+                          <div style="height: 2rem;"></div>
+                        </div>
+                        <div class="table_cell">
+                          <small style="text-transform:uppercase;display:block;text-align:center;background-color:gray;font-size:10px"><strong>Distinction </strong></small> 
+
+                          <small style="text-transform:uppercase;font-size:9px;display:inline-block;width:100%;">Félicitation : <span style="font-weight:bold;">........................</span>                </small> 
+                          <small style="text-transform:uppercase;font-size:9px;display:inline-block;width:100%;position: relative;">Encouragement  :                  <span style="font-weight:bold;">..................</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;display:inline-block;width:100%;">Tableau d'honneur :  <span style="font-weight:bold;">..............</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;display:inline-block;width:100%;position: relative;">Avertissement :      <span style="font-weight:bold;">........................</span></small> 
+                          <small style="text-transform:uppercase;display:block;font-size:9px;display:inline-block;width:100%;">Blame :              <span style="font-weight:bold;">........................</span></small> 
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="table_cell">
+                    
+                    <small style="text-transform:uppercase;display:block;text-align:center;font-size:10px"><strong>Le directeur</strong></small> 
+                    <div style="height:5rem"></div>
+                    <small style="text-transform:uppercase;display:block;text-align:right"><strong>Lomé le, 04/09/2023</strong></small> 
+                  </div>
                 </div>
-            </div>
+              </div>
+              
         </div>
       </div>
       
